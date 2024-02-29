2a22c4feb9771661689704657b65de334bb12672
Prajwal Singh <prajwalrishabh@gmail.com>
2024-02-29 23:21:30 +0530

    Update config.py

--- EEGClip/config.py
+++ EEGClip/config.py
@@ -10,13 +10,13 @@ torch.manual_seed(45)
 
 
 base_path       = '../data/'
-# train_path      = 'eeg_imagenet40_cvpr_2017_raw/train/'
-# validation_path = 'eeg_imagenet40_cvpr_2017_raw/val/'
-# test_path       = 'eeg_imagenet40_cvpr_2017_raw/test/'
+train_path      = 'eeg_imagenet40_cvpr_2017_raw/train/'
+validation_path = 'eeg_imagenet40_cvpr_2017_raw/val/'
+test_path       = 'eeg_imagenet40_cvpr_2017_raw/test/'
 
-train_path      = 'eeg_imagenet40_cvpr_2017_5_45hz/train/'
-validation_path = 'eeg_imagenet40_cvpr_2017_5_45hz/val/'
-test_path       = 'eeg_imagenet40_cvpr_2017_5_45hz/test/'
+# train_path      = 'eeg_imagenet40_cvpr_2017_5_45hz/train/'
+# validation_path = 'eeg_imagenet40_cvpr_2017_5_45hz/val/'
+# test_path       = 'eeg_imagenet40_cvpr_2017_5_45hz/test/'
 
 device          = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # device = 'cpu'
@@ -29,7 +29,7 @@ hidden_size    = embedding_dim//2
 num_layers     = 1
 batch_size     = 64
 test_batch_size= 64
-# epoch          = 2049
-epoch          = 251 # for EEG fine-tuning
+epoch          = 2049
+# epoch          = 251 # for EEG fine-tuning
 # epoch          = 10 # for image fine-tuning
 temperature    = 0.5
